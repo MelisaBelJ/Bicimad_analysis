535b65fb2dd5cd145157942861054111e6db814a
Melisa <123397651+MelisaBelJ@users.noreply.github.com>
2023-05-04 11:46:21 +0200

    Comentarios

--- practica_4.py
+++ practica_4.py
@@ -11,25 +11,35 @@ path = 'DatosBICIMAD'
 estaciones = 'Estaciones.json'
 pathYear = lambda y: path+f'/BiciMAD_{y}'
 
+#Clase para centralizar las funciones sobre el dataFrame
 class Datos():
     def __init__(self, df):
         self.df = df
 
+#para mostrar por pantalla la tabla del dataFrame
+#Si noEntero es True entonces solo se muestran los 20 primero en vez de la tabla completa
+#Si es False se muestra completa
     def muestra(self, noEntero = True):
         self.df.show(20 if noEntero else self.df.count(), False)
-    
+ 
+#Muestra por pantalla la media, el mínimo y el máximo de los datos   
     def describe(self):
         self.df.describe().show()
-        
+  
+#devuelve un dataFrame con una columna nombre con los elementos de esa columna
+#y otra columna count con el total de estos en el dataframe     
     def cantidadEngrupo(self, nombre):
         return Datos(self.df.groupBy(nombre).count())
-        
-   #Nos quedamos con los viajes desde o hasta una de las estaciones
+
+ #devuelve un dataframe con los viajes desde o hasta Estacion
     def filtraEstaciones(self, Estacion):
         dfCU = self.df.filter(F.col("Estacion_Salida" ).contains(Estacion) 
                        | F.col("Estacion_Llegada").contains(Estacion))
         return Datos(dfCU)
 
+#devuelve un gráfico hecho con los datos de nombreX en el eje X y los 
+# de nombreY en el Y. 
+#Si barras es True, el gráfico es de barras
     def grafico(self, nombreX, nombreY, barras=True):
         x,y=[],[]
         for vx,vy in self.df.select(nombreX, nombreY).collect():
@@ -46,7 +56,9 @@ class Datos():
         plt.legend([nombreX], loc='upper left')
 
         plt.show()
-        
+
+#Cambia idunplug_station e idplug_station por el nombre de la estación correspondiente
+# bajo el nuevo nombre de "Estacion_Llegada" y "Estacion_Salida"        
     def formateaEstaciones(self):
         #Con los nombres de las estaciones, que se ve mejor :)
         df2 = self.spark.read.json(f'{path}/{estaciones}',  multiLine=True)
@@ -56,6 +68,7 @@ class Datos():
         df3 = df3.drop('idunplug_station','idplug_station')
         return Datos(df3)
 
+#Extension de datos para la consulta inicial
 class Consulta(Datos):    
     def __init__(self, nombres):
         self.spark = SparkSession.builder.getOrCreate()        
@@ -87,22 +100,12 @@ posibles valores son:
 2: Usuario ocasional
 3: Trabajador de la empresa
 6, 7: ¿? 
-
-rangoEdad: Número que indica el rango de edad del usuario que ha realizado el
-movimiento. Sus posibles valores son:
-0: No se ha podido determinar el rango de edad del usuario
-1: El usuario tiene entre 0 y 16 años
-2: El usuario tiene entre 17 y 18 años
-3: El usuario tiene entre 19 y 26 años
-4: El usuario tiene entre 27 y 40 años
-5: El usuario tiene entre 41 y 65 años
-6: El usuario tiene 66 años o más
 """
 
-#Para descargar los datos la primera vez que lo usamos (Tarda un rato...)             
-#Leemos todos los ficheros json de year
+#Para leer los datos de un año y producir los resultados indicados en el readme
 def datos(nEst, y):
     pathY = pathYear(y)
+    #Descargamos los datos si no están ya
     if not os.path.isdir(path) or not 'Estaciones.json' in os.listdir(path):
         print('Descargando Datos Estaciones, puede tardar')
         url = "https://drive.google.com/drive/folders/1dqnPVK-5qzsJJarUBwj-xcOIWjjUtpBz"
@@ -110,6 +113,8 @@ def datos(nEst, y):
     if not os.path.isdir(pathY):
         print('Descargando Datos Año, puede tardar')
         descargaY(y)
+
+    #Hacemos la consulta para generar el dataframe del año
     consulta = Consulta([f'{pathY}/{item}' for item in os.listdir(pathYear) if item.endswith('.json')])
 
     print(f'Viajes hecho por cada tipo de usuario, por alguna razón aparecen números 6 y 7 que no están definidos en la documentación oficial, año {y}')
@@ -136,10 +141,13 @@ def datos(nEst, y):
 
     consulta.spark.stop()
 
-import sys
+#Si se ejecuta sin argumentos da los datos de Sol en 2020
+#Si tiene 1 argumento, los de la estación q se ha dado como argumento en 2020
+#Si tiene 2 o más, da los datos de la estación q se ha dado en el primer argumento
+#En los años de los sucesivos argumentos (del 2 en delante)
 if __name__=="__main__":
     l = len(sys.argv)
-    nEst = 'Barajas' if l<1 else sys.argv[1]
+    nEst = 'Sol' if l<1 else sys.argv[1]
     x = 2
     while l>x:
         y = sys.argv[x]
